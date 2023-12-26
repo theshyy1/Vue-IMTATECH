cb51c3aebf87b481beec98d0cf6cdf000a6f61c3
theshyy1 <ronaldoisbestt@gmail.com>
2023-12-26 16:15:20 +0700

    First commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterView } from "vue-router";
+import BaseLayout from "./layouts/BaseLayout.vue";
+</script>
+
+<template>
+  <div class="">
+    <BaseLayout>
+      <h1>Hello world</h1>
+    </BaseLayout>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/layouts/BaseLayout.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { RouterView } from "vue-router";
+
+import Footer from "../components/Footer.vue";
+import Header from "../components/Header.vue";
+</script>
+
+<template>
+  <Header></Header>
+  <slot></slot>
+  <Footer></Footer>
+</template>
