2f5bd14b2272f52b514d50faafcd2cae013027b0
theshyy1 <ronaldoisbestt@gmail.com>
2023-12-29 18:33:02 +0700

    update: profile, edit profile

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,16 @@ const routes = [
     name: "Home",
     component: () => import("../view/HomePage.vue"),
   },
+  {
+    path: "/profile",
+    name: "Profile",
+    component: () => import("../view/Profile.vue"),
+  },
+  {
+    path: "/profile/:id/edit",
+    name: "Profile Edit",
+    component: () => import("../view/EditProfile.vue"),
+  },
 ];
 
 const router = createRouter({
--- src/view/Profile.vue
+++ src/view/Profile.vue
@@ -1,7 +1,8 @@
 <script setup>
-import { ref } from "vue";
+import { computed, ref } from "vue";
 
 const user = ref({
+  id: 1,
   name: "Nguyen Van A",
   birthDay: "18/02/1999",
   gender: "Male",
@@ -10,10 +11,105 @@ const user = ref({
   email: "hoangtrung@gmail.com",
   address: "Ha Noi, Viet Nam",
 });
+
+const birthday = computed(() =>
+  user.birthDay ? user.birthDay.split("-") : ["01", "01", "2000"]
+);
+
+const hidePhoneNumber = computed(() => {
+  const newNumber = ref("");
+
+  if (user.phone) {
+    newNumber.value = user.phone.slice(0, -4).padEnd(user.phone.length, "*");
+  }
+  return newNumber.value;
+});
 </script>
 
 <template>
-  <div class="">
-    <h1>Profile</h1>
+  <div class="container">
+    <div class="flex flex-col justify-center items-center">
+      <h1 class="my-7 text-3xl font-bold text-sky-500">Profile Cá Nhân</h1>
+      <RouterLink
+        v-if="user.role === '1'"
+        :to="`/edit/profile/${user.id}/edit`"
+        class="underline absolute top-8 right-[100px] hover:text-red-600"
+        ><i class="fa-solid fa-gear"></i> Thay đổi thông tin</RouterLink
+      >
+      <RouterLink
+        v-else
+        :to="`/profile/${user.id}/edit`"
+        class="underline absolute top-8 right-[100px] hover:text-red-600"
+        ><i class="fa-solid fa-gear"></i> Thay đổi thông tin</RouterLink
+      >
+      <div class="">
+        <img
+          :src="user?.image || 'https://picsum.photos/200/200'"
+          class="rounded-full w-[200px] h-[200px] object-cover"
+          alt=""
+        />
+      </div>
+      <div class="text-center my-4 max-w-[400px]">
+        <span class="italic text-base opacity-60">{{
+          user.description || ""
+        }}</span>
+      </div>
+    </div>
+    <div
+      class="w-[600px] mx-auto border-[1px] rounded shadow-md text-sm space-y-4 border-white py-4 px-6"
+    >
+      <p class="text-xl shadow-sm">Thông tin cá nhân</p>
+      <div
+        class="flex justify-start mt-[60px] items-center ml-[40px] text-base"
+      >
+        <ul class="flex flex-col items-end mr-4 space-y-4 text-neutral-600">
+          <li>Tên đăng nhập</li>
+          <li>Tên</li>
+          <li>Giới tính</li>
+          <li>Ngày sinh</li>
+          <li>Địa chỉ</li>
+          <li>Số điện thoại</li>
+        </ul>
+        <ul class="flex flex-col items-start space-y-4 box-border">
+          <li>
+            <p>{{ user.email || "" }}</p>
+          </li>
+          <li>
+            <p>{{ user.name || "" }}</p>
+          </li>
+          <li>
+            <p>
+              <input
+                type="radio"
+                value="1"
+                :checked="user.gender === '1'"
+                :disabled="user.gender !== '1'"
+              />Nam
+              <input
+                type="radio"
+                value="2"
+                :checked="user.gender === '2'"
+                :disabled="user.gender !== '2'"
+              />Nữ
+
+              <input
+                type="radio"
+                value="0"
+                :disabled="user.gender !== '0'"
+              />Khác
+            </p>
+          </li>
+          <li>
+            {{ `${birthday[0]} / ${birthday[1]} / ${birthday[2]}` }}
+          </li>
+          <li>
+            <span class="">{{ user.address || "..." }}</span>
+          </li>
+          <li>
+            <span class="">{{ hidePhoneNumber || "..." }}</span>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
